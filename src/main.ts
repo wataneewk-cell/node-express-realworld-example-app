5caa20d86b4e3fa227b7a19cb017000e43b09e63
watanee <watanee.wk@gmail.com>
2026-09-04 12:45:03 +0700

    edit

--- src/main.ts
+++ src/main.ts
@@ -52,6 +52,6 @@ app.use(
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.info(`server up on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
 });
